f5f63bef476b747787fa0c9952f80c6648c71371
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2024-12-22 19:39:21 +0100

    added new config

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const authRoute = require("./routes/auth.route");
-const PORT = process.env.PORT || 5000;
 const db = require("./config/db");
 const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-// middlewares
+// Middlewares
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-app.use(errorHandler);
-// routes
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Routes
 app.use("/api/v1/auth", authRoute);
 
 app.get("/", (req, res) => {
-  res.send("Welcome Cute container server");
+  res.status(200).json({
+    message: "Welcome to the Cute Container server",
+  });
 });
 
-// connect to db
-db();
-app.listen(PORT, () => {
-  console.log("Server is Ready");
-});
+app.use(errorHandler);
+
+db()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error.message);
+    process.exit(1);
+  });
